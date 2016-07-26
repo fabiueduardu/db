057473c2a077a4463165c6b3dfe13a733d4bdaeb
Fabio Eduardo <fabiueduardu@hotmail.com>
2016-07-26 00:22:57 -0300

    test rid and key lookup in select

---
+++ src/sql_server/performance/sql_index_key_lookup.sql
@@ -0,0 +1,35 @@
+--### table with PK
+--### drop table [lab_46671C82_user]
+create table [lab_46671C82_user](userid int identity(1,2) primary key, name varchar(100) , email varchar(100))
+
+--### create index
+create index IX1lab_46671C82_user on [lab_46671C82_user](Email)
+	
+--### data
+--### truncate table [lab_46671C82_user]
+declare @i int = ISNULL((select max(userid)+1 from [lab_46671C82_user]),1)
+declare @itotal int = @i + 100000
+
+while @i<@itotal
+begin 
+
+	insert into [lab_46671C82_user] values('Name ' + cast(@i as varchar) , cast(@i as varchar)+'@com.com')
+	set @i = @i + 1
+end
+
+--### data view
+	select top 10 * from [lab_46671C82_user] order by userid desc
+	select top 10 * from [lab_46671C82_user]
+	select top 10 * from [lab_46671C82_user] order by newid()
+	select count(*) from [lab_46671C82_user]
+	select max(userid) from [lab_46671C82_user]
+
+---### use index clustered and nonclusred(email) with key lookup
+	select  * from [lab_46671C82_user] where userid = 195659
+	select  * from [lab_46671C82_user] where name = 'Name 97830'
+	select  * from [lab_46671C82_user] where email = '97830@com.com'
+
+---### use index clustered and nonclusred(email) without key lookup
+	select  userid from [lab_46671C82_user] where userid = 195659
+	select  name from [lab_46671C82_user] where name = 'Name 97830'
+	select  email from [lab_46671C82_user] where email = '97830@com.com'
