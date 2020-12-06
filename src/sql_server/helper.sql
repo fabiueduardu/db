aa802906dc8c3a250562594e24cd81cd28373517
Fabio Eduardo <fabiueduardu@hotmail.com>
2020-12-06 15:07:08 -0300

    Update helper.sql

--- src/sql_server/helper.sql
+++ src/sql_server/helper.sql
@@ -482,3 +482,21 @@ FROM sys.stats AS stat
 		JOIN sys.tables t
 			ON stat.object_id = OBJECT_ID(t.name)
 order by t.name, stat.name , last_updated desc
+
+
+
+## “The transaction log for database is full due to 'LOG_BACKUP'” 
+
+USE {database-name};  
+GO  
+-- Truncate the log by changing the database recovery model to SIMPLE.  
+ALTER DATABASE {database-name}
+SET RECOVERY SIMPLE;  
+GO  
+-- Shrink the truncated log file to 1 MB.  
+DBCC SHRINKFILE ({database-file-name}, 1);   --LOG file NAME
+GO  
+-- Reset the database recovery model.  
+ALTER DATABASE {database-name}
+SET RECOVERY FULL;  
+GO 
